fcca418b9df16df9167e8d42950283d67371b196
“[Adekomi <adekomiabdulbaqi3@gmail.com>
2020-10-22 19:26:25 +0100

    functionality updated to support bitcoin conversion to usd

--- lib/coin_data.dart
+++ lib/coin_data.dart
@@ -1,3 +1,6 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
 const List<String> currenciesList = [
   'AUD',
   'BRL',
@@ -22,10 +25,25 @@ const List<String> currenciesList = [
   'ZAR'
 ];
 
+const coinAPIURL = 'https://rest.coinapi.io/v1/exchangerate';
+const apiKey = '784B9C56-9BF8-4542-8C07-C554027CAE74';
+
 const List<String> cryptoList = [
   'BTC',
   'ETH',
   'LTC',
 ];
 
-class CoinData {}
+class CoinData {
+  Future getCoinData() async {
+    String requestURL = '$coinAPIURL/BTC/USD?apikey=$apiKey';
+    http.Response response = await http.get(requestURL);
+    if (response.statusCode == 200) {
+      var decodedData = jsonDecode(response.body);
+      var lastPrice = decodedData['rate'];
+      return lastPrice;
+    } else {
+      print(response.statusCode);
+    }
+  }
+}
